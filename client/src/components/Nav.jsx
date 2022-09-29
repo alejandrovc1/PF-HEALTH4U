1e82fe7313c5ff10e0467134525fc978c4dcc3b3
Santiago-Miranda <100096337+Santiago-Miranda@users.noreply.github.com>
2022-09-28 23:51:29 -0300

    first commit

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -13,17 +13,14 @@ export default function Nav(){
                 <div>
                    <ul id={style.navul}>
                       <li><a href="index.html">Specialties</a></li>
-                      <li><a href="index.html">Contact Us</a></li>
-                      <li><a href="index.html">Profile</a></li>
+                      <li><a href="index.html">About us</a></li>
+                      <li><a href="index.html">Cart</a></li>
                       <li><a href="index.html">Sign Up</a></li>
                       <button className={style.boton}>Sign in</button>
                     </ul>
                 </div>
              </div>
             </nav>
-            <div id="mobile">
-                 <i className="fas fabars"></i>
-            </div>
          </header>
     )
 }
