6a5d66d338c6178b2dc42de8f291b0379bfcd843
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-10-01 02:14:09 -0500

    css del form

--- client/src/components/HomeDocLogged.jsx
+++ client/src/components/HomeDocLogged.jsx
@@ -17,9 +17,9 @@ export default function HomeDocLogged()
             <div>
                 <p>HOLA</p>
             </div>
-            <div>
-                <Footer />
-            </div>
+
+            <Footer />
+
         </div>
     )
 }
--- client/src/components/NavDoctorLogged.jsx
+++ client/src/components/NavDoctorLogged.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Nav.module.css";
 import logo from "./image/logo.png";
+import { Route, Link } from "react-router-dom";
 
 
 export default function NavDoctorLogged()
@@ -14,7 +15,9 @@ export default function NavDoctorLogged()
                     <div>
                         <ul id={style.navul}>
                             <li><a href="index.html">My Schedule</a></li>
-                            <li><a href="index.html">Account</a></li>
+                            <div>
+                                <Link to="/account" className={style.account}>Account</Link>
+                            </div>
                             <li><a href="index.html">My Finances</a></li>
                             <li><a>Hello, Dr. </a></li>
                             <button className={style.boton}>SIGN OUT</button>
