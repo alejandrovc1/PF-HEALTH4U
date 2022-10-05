9f1e8a71916ef297f5c62886eb66310bd4a0f06c
AlejandroHELO <alejohl0799@gmail.com>
2022-10-05 13:26:03 -0500

    Para que no vuelva a joder React

--- api/src/db.js
+++ api/src/db.js
@@ -11,13 +11,12 @@ const dbConn = () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-        //devuelve promesa
-        .then(() => {
-            console.log('Database connected to ATLAS')
-        }).catch(err => {
-            console.error('Connection Error: ',err)
-        })
+    //devuelve promesa
+    .then(() => {
+        console.log('Database connected to ATLAS')
+    }).catch(err => {
+        console.error('Connection Error: ',err)
+    })
 };
-
 
 module.exports = { dbConn }
