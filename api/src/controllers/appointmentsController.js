b5a30379f2bf715f8f5f98ed6b35cb72e733e310
Makami-Yv <attercoppe@protonmail.com>
2022-10-15 20:44:00 -0500

    Appointment Backend para citas

--- api/src/controllers/appointmentsController.js
+++ api/src/controllers/appointmentsController.js
@@ -1,21 +1,28 @@
 const appointmentModel = require('../models/appointment')
 // const mongoose = require("mongoose");
 
-const createAppointment = async (req, res, next) => {
-    let {service, date, doctor} = req.body
-
+const createAppointments = async (req, res, next) => {
+    let { start, end, doctor } = req.body
     //date = date.toLocaleString()
-
-    if ( !service || !date || !doctor ) res.status(400).send('Falta enviar datos obligatorios');
-    else if (typeof service !== 'string' || typeof doctor !== "string") {
-    res.status(400).send("Error, los tipos de datos son incorrectos")}
-
+    
+    if ( !start || !end || !doctor ) res.status(400).send('Falta enviar datos obligatorios');
+    // else if (typeof service !== 'string' || typeof doctor !== "string") {
+    // res.status(400).send("Error, los tipos de datos son incorrectos")}
+    
     try {
-        await appointmentModel.create({
-            service,
-            date,
-            doctor
-        });
+        const foundedAppo = await appointmentModel.find({ start: start })
+        const existingAppo = foundedAppo.filter(a => a.doctorID === doctor)
+        if(existingAppo.length > 0) {
+            res.status(400).send('Appointment already exists')
+        } else {
+            await appointmentModel.create({
+                start,
+                end,
+                status: "Free",
+                doctorID: doctor
+            });
+            res.status(200).send('Appointment Successfully Created');
+        }
 
         // let crearConsulta = await new appointmentModel({})
         // crearConsulta.save().then( result => {
@@ -24,60 +31,126 @@ const createAppointment = async (req, res, next) => {
         // })
         // .catch(err => next(err))
 
-        res.status(200).send('Appointment Successfully Created');
     } catch(error) {
         console.log('Error creating the appointment');
         next(error)
     }
 };
 
-const showAppointments = async (req, res, next) => {
+const getAppointments = async () => {
     try{
-        await appointmentModel.find({})
-
-        .then( result => {
-            res.status(200).json(result)
-            // mongoose.connection.close()
+        const response = await appointmentModel.find({})
+        const Appointments = response?.map(a => {
+            const Appoint = {
+                id: a._id,
+                start: a.start,
+                end: a.end,
+                status: a.status,
+                doctorID: a.doctorID,
+                patientID: a.patientID
+            } 
+            return Appoint;
         })
-        .catch(err => next (err))
+        if (Appointments.length > 0) {
+            return Appointments
+        } else return { msg: "There is no Appointments" }
+
     } catch (error) {
         console.log("Appointments couldn't be shown");
-        next(error)
+        throw new Error("An Error Ocurred. There's no Appointments")
     }
 };
 
-const deleteAppointment = async (req, res, next) => {
+const getAppointmentByDoctor = async (doctor) => {
     try {
-        const {id} = req.params
-
-        await appointmentModel.findByIdAndRemove(id)
-        .then( () => {
-            res.status(200).send("Appointment Successfully Deleted")
+        const response = await appointmentModel.find({ doctorID: doctor })
+        const Appointments = response?.map(a => {
+            const Appoint = {
+                id: a._id,
+                start: a.start,
+                end: a.end,
+                status: a.status,
+                doctorID: a.doctorID,
+                patientID: a.patientID
+            } 
+            return Appoint;
         })
-    } catch (error) { next(error)}
-};
+        if (Appointments.length > 0) {
+            return Appointments
+        } else return { msg: "There is no Appointments with that doctor ID" }
+
+
+    } catch (e) {
+        console.error(e);
+        throw new Error("An Error Ocurred. There's no Appointments with that doctor ID")
+    }
+}
+
+const getAppointmentByPatient = async (patient) => {
+    try {
+        const response = await appointmentModel.find({ patientID: patient })
+        const Appointments = response?.map(a => {
+            const Appoint = {
+                id: a._id,
+                start: a.start,
+                end: a.end,
+                status: a.status,
+                doctorID: a.doctorID,
+                patientID: a.patientID
+            } 
+            return Appoint;
+        })
+        if (Appointments.length > 0) {
+            return Appointments
+        } else return { msg: "There is no Appointments with that patient ID" }
+
+
+    } catch (e) {
+        console.error(e);
+        throw new Error("An Error Ocurred. There's no Appointments with that patient ID")
+    }
+}
 
 const updateAppointment = async (req, res, next) => {
     try {
-        const {id} = req.params
-        const {service, date, doctor} = req.body
+        const { start, patient } = req.body
+        const idAppo = await appointmentModel.findOne({ start: start })
+        if(!idAppo) {
+            res.status(400).send("Can't find the Appointment")
+        } else {
+            const updatedAppo = await appointmentModel.findByIdAndUpdate(idAppo._id, {
+                status: "Occupied",
+                patientID: patient
+            }, { new : true}) // este ultimo parámetro hace que nos devuelva el doc actualizado
+            .then( () => {
+                res.status(200).send("Appointment Successfully Updated")
+            })
+        }
+        
+    } catch (error) { next(error)}
+};
 
-        const updatedAppo = await appointmentModel.findByIdAndUpdate(id, {
-            service: service,
-            date: date,
-            doctor: doctor
-        }, { new : true}) // este ultimo parámetro hace que nos devuelva el doc actualizado
-        .then( () => {
-            console.log(updatedAppo)
-            res.status(200).send("Appointment Successfully Updated")
-        })
-    
+const cancelAppointment = async (req, res, next) => {
+    try {
+        const { id } = req.body
+        if(id){
+            await appointmentModel.findByIdAndUpdate(id, {
+                status: "canceled"
+            })
+            .then( () => {
+                res.status(200).send("Appointment Successfully Canceled")
+            })
+        } else {
+            res.status(400).send("Appointment cant be Cancelled")
+        }
     } catch (error) { next(error)}
 };
 
 module.exports = {
-    createAppointment,
-    showAppointments,
-    deleteAppointment,
-    updateAppointment
+    createAppointments,
+    getAppointments,
+    getAppointmentByDoctor,
+    getAppointmentByPatient,
+    updateAppointment, 
+    cancelAppointment 
 }
--- api/src/routes/appointments.js
+++ api/src/routes/appointments.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const { isPatient, verifySignUp } = require('../middlewares/index')
+//const { isPatient, verifySignUp } = require('../middlewares/index')
 
-const { createAppointment, showAppointments, deleteAppointment, updateAppointment} = require('../controllers/appointmentsController');
+const { 
+    createAppointments, 
+    getAppointments, 
+    getAppointmentByDoctor,
+    getAppointmentByPatient,
+    updateAppointment, 
+    cancelAppointment 
+} = require('../controllers/appointmentsController');
 
-router.get('/', [isPatient,],showAppointments);
-router.post('/', createAppointment);
-router.delete('/:id', deleteAppointment);
-router.put('/:id', updateAppointment);
+router.get('/', async (req, res, next) => {
+    try{
+        const { doctor } = req.query
+        const { patient } = req.query
+        if(doctor) {
+            const response = await getAppointmentByDoctor(doctor)
+            return res.status(200).send(response)
+        } else if(patient) {
+            const response = await getAppointmentByPatient(patient)
+            return res.status(200).send(response)
+        } else {
+            const response = await getAppointments()
+            return res.status(200).send(response)
+        }
+
+
+    } catch (e) {
+        console.error(e);
+        return res.status(400).send("An Error Ocurred. There's no Appointments")
+        next(e)
+    }
+});
+router.post('/create', createAppointments);
+router.put('/update', updateAppointment);
+router.put('/cancel', cancelAppointment);
 
 module.exports = router;
