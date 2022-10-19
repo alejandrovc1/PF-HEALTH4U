c267cd61e05dfc870b758ec3a0a2a672a58e85c2
matalala <smitesotra@gmail.com>
2022-10-19 15:27:46 -0300

    credenciales firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,13 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBooxWVCFrDriYydNtGEGSxUOtwPUwG6X0",
-  authDomain: "react-auth-pf.firebaseapp.com",
-  projectId: "react-auth-pf",
-  storageBucket: "react-auth-pf.appspot.com",
-  messagingSenderId: "917804735198",
-  appId: "1:917804735198:web:e18e28e6e71e054fad404f"
+  apiKey: "AIzaSyBKlR11_6BhfL9KwIY6RdujPo4C-XfH9Js",
+  authDomain: "healt4u-327ee.firebaseapp.com",
+  projectId: "healt4u-327ee",
+  storageBucket: "healt4u-327ee.appspot.com",
+  messagingSenderId: "428289733864",
+  appId: "1:428289733864:web:73841c6d6f1330e493c55d",
+  measurementId: "G-5VEWWFEQCP"
 };
 
 // Initialize Firebase
